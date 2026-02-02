f0a715ec346abf94940c3e4a3a2c983f4ea5b9ba
karim <karimalaa298@gmail.com>
2026-02-02 09:02:25 +0300

    finish work with fetch data from firebase and fix issues in app when reload make post request to the firebase

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
-
+import {fetchCartData, sendCartData} from './store/cart-actions';
+ 
 let isInitial = true;
 
 function App() {
@@ -22,45 +22,69 @@ function App() {
    * We'll fix this over the next lectures, I just wanted to point it out here!
    */
 
+
+  //Version #1 Use useEffect insinde the component and make http request
+  // useEffect(() => {
+  //   const sendCartData = async () => {
+  //     dispatch(uiActions.showNotification({
+  //       status: 'pending',
+  //       title: 'Sending...',
+  //       message: 'Sending cart data!',
+  //     }));
+
+  //     const response = await fetch('https://react-app-1-78d61-default-rtdb.firebaseio.com/cart.json',{
+  //       method:'PUT',
+  //       body: JSON.stringify(cart)      
+  //     });
+
+  //     if(!response.ok){
+  //       throw new Error('Sending cart data failed!');
+  //     }
+  //     const responseData = response.json();
+  //     dispatch(uiActions.showNotification({
+  //         status: 'success',
+  //         title: 'Success!',
+  //         message: 'Sent cart data successfully!',
+  //     }));      
+  //   }
+
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return;
+  //   }
+
+  //   sendCartData().catch((error) =>{
+  //     dispatch(uiActions.showNotification({
+  //       status: 'error',
+  //       title: 'Error!',
+  //       message: 'Sending cart data failed!',        
+  //     }));
+  //   });
+
+    
+  // },[cart, dispatch]);
+
+  //used to fetch data onec the application is loaded
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      }));
+    dispatch(fetchCartData())
+  }, [dispatch]);
 
-      const response = await fetch('https://react-app-1-78d61-default-rtdb.firebaseio.com/cart.json',{
-        method:'PUT',
-        body: JSON.stringify(cart)      
-      });
-
-      if(!response.ok){
-        throw new Error('Sending cart data failed!');
-      }
-      const responseData = response.json();
-      dispatch(uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-      }));      
-    }
-
+    //Version #2 Use useEffect insinde the component and 
+    // move the http request to the action creator in redux toolkit
+    //second pattern which depend on something called Thunk
+    useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) =>{
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!',        
-      }));
-    });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }    
 
-    
+    // dispatch(sendCartData(cart))
   },[cart, dispatch]);
+
 
   return (
     <>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,15 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {items: [], totalQuantity: 0};
+const initialState = {
+    items: [], 
+    totalQuantity: 0,
+    changed: false,
+};
 
 const cartSlice = createSlice({
     name:'cart',
     initialState : initialState,
     reducers:{
+        replaceCart : (state,actions) => {
+            state.totalQuantity = actions.payload.totalQuantity;
+            state.items = actions.payload.items;
+        },
         addItemToCart:(state, action) => {
             const newItem = action.payload;
             const exsitingItem = state.items.find((item) => item.id === newItem.id);
             state.totalQuantity++;
+            state.changed = true;
             if(!exsitingItem){
                 state.items.push({
                     id: newItem.id,
@@ -27,6 +36,7 @@ const cartSlice = createSlice({
             const id = action.payload;
             const exsitingItem = state.items.find((item) => item.id === id);
             state.totalQuantity--;
+            state.changed = true;
             if(exsitingItem.quantity === 1){
                 state.items = state.items.filter((item) => item.id !== id);               
             }else{
@@ -36,6 +46,8 @@ const cartSlice = createSlice({
         }
     }
 });
+
+
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
